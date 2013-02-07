d5f5457098b3c7e0d960daa7ea30216a793a6bdb
Peter Hofmann <scminsecure@uninformativ.de>
2013-02-07 18:50:18 +0100

    Firefox: Do not use DNS cache...

--- FirefoxTempBase/prefs.js
+++ FirefoxTempBase/prefs.js
@@ -51,6 +51,7 @@ user_pref("general.warnOnAboutConfig", false);
 user_pref("intl.charsetmenu.browser.cache", "UTF-8");
 user_pref("layout.spellcheckDefault", 0);
 user_pref("network.cookie.prefsMigrated", true);
+user_pref("network.dnsCacheExpiration", 0);
 user_pref("places.history.expiration.transient_current_max_pages", 13264);
 user_pref("privacy.cpd.offlineApps", true);
 user_pref("privacy.donottrackheader.enabled", true);
