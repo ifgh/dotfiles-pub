d120839f54622be57c581e693239dc6380917bd4
Peter Hofmann <scm@uninformativ.de>
2013-06-02 21:13:12 +0200

    dwm: Let xterm-lock float
    Otherwise, there is a short period of time where it will get tiled. This
    results in all other windows being resized... Some terminals may lose
    part of their output!

--- dwm-config.h
+++ dwm-config.h
@@ -35,6 +35,7 @@ static const Rule rules[] = {
 	{ "Firefox",        NULL,                   NULL,       1 << (1 - 1),  False,       -1 },
 	{ "XTerm",          "xterm-tac",            NULL,       1 << (2 - 1),  False,       -1 },
 	{ "XTerm",          "xterm-floating",       NULL,       0,             True,        -1 },
+	{ "XTerm",          "xterm-lock",           NULL,       0,             True,        -1 },
 	{ "qemu",           NULL,                   NULL,       0,             True,        -1 },
 	{ "Avidemux2_gtk",  NULL,                   NULL,       0,             True,        -1 },
 	{ "feh",            NULL,                   NULL,       0,             True,        -1 },
