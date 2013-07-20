939fb5e71df22037886d7b548302a7e51535b7df
Peter Hofmann <scm@uninformativ.de>
2013-07-20 11:16:29 +0200

    dwm: Fix spacing of "rules"

--- dwm-config.h
+++ dwm-config.h
@@ -29,22 +29,22 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance                title       tags mask      isfloating   monitor */
-	{ "Avidemux2_gtk",  NULL,                   NULL,       0,             True,        -1 },
-	{ "Firefox",        NULL,                   NULL,       1 << (1 - 1),  False,       -1 },
-	{ "Gitk",           NULL,                   NULL,       0,             True,        -1 },
-	{ "Hgk",            NULL,                   NULL,       0,             True,        -1 },
-	{ "libreoffice-calc", NULL,                 NULL,       0,             True,        -1 },
-	{ "libreoffice-writer", NULL,               NULL,       0,             True,        -1 },
-	{ "MPlayer",        NULL,                   NULL,       0,             True,        -1 },
-	{ "qemu",           NULL,                   NULL,       0,             True,        -1 },
-	{ "VirtualBox",     NULL,                   NULL,       0,             True,        -1 },
-	{ "Wine",           NULL,                   NULL,       0,             True,        -1 },
-	{ "Xdialog",        NULL,                   NULL,       0,             True,        -1 },
-	{ "Xpdf",           NULL,                   NULL,       0,             True,        -1 },
-	{ "XTerm",          "xterm-floating",       NULL,       0,             True,        -1 },
-	{ "XTerm",          "xterm-lock",           NULL,       0,             True,        -1 },
-	{ "XTerm",          "xterm-tac",            NULL,       1 << (2 - 1),  False,       -1 },
+	/* class                 instance           title  tags mask      isfloating  monitor */
+	{ "Avidemux2_gtk",       NULL,              NULL,  0,             True,       -1 },
+	{ "Firefox",             NULL,              NULL,  1 << (1 - 1),  False,      -1 },
+	{ "Gitk",                NULL,              NULL,  0,             True,       -1 },
+	{ "Hgk",                 NULL,              NULL,  0,             True,       -1 },
+	{ "libreoffice-calc",    NULL,              NULL,  0,             True,       -1 },
+	{ "libreoffice-writer",  NULL,              NULL,  0,             True,       -1 },
+	{ "MPlayer",             NULL,              NULL,  0,             True,       -1 },
+	{ "qemu",                NULL,              NULL,  0,             True,       -1 },
+	{ "VirtualBox",          NULL,              NULL,  0,             True,       -1 },
+	{ "Wine",                NULL,              NULL,  0,             True,       -1 },
+	{ "Xdialog",             NULL,              NULL,  0,             True,       -1 },
+	{ "Xpdf",                NULL,              NULL,  0,             True,       -1 },
+	{ "XTerm",               "xterm-floating",  NULL,  0,             True,       -1 },
+	{ "XTerm",               "xterm-lock",      NULL,  0,             True,       -1 },
+	{ "XTerm",               "xterm-tac",       NULL,  1 << (2 - 1),  False,      -1 },
 };
 
 /* layout(s) */
